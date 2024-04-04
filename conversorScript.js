e5cb1ed322d1f601a51cd9ff962fe81c91a10225
sales16 <eduardo.sales.silva1603@gmail.com>
2024-04-04 10:48:37 -0300

    ChicoMoedas

---
+++ conversorScript.js
@@ -0,0 +1,44 @@
+let valoresConversao = {
+    real: {
+        dolar: 0.27,
+        euro: 0.18,
+        real: 1
+    },
+    dolar: {
+        real: 5.03,
+        euro: 1.09,
+        dolar: 1
+    },
+    euro: {
+        real: 5.47,
+        dolar: 0.92,
+        euro: 1
+    }
+}
+
+function converter() {
+    let valorUsuario = document.getElementById("valor-usuario").value;
+    let moedaOrigem = document.getElementById("moeda1").value;
+    let moedaDestino = document.getElementById("moeda2").value;
+    let paragrafoResultado = document.getElementById("resultado");
+
+    let conversao = valorUsuario * valoresConversao[moedaOrigem][moedaDestino]
+
+
+    if (moedaDestino == "dolar") {
+        paragrafoResultado.textContent = conversao.toLocaleString('pt-BR', { style: 'currency', currency: 'USD' });
+    } else if (moedaDestino == "real") {
+        paragrafoResultado.textContent = conversao.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    } else {
+        paragrafoResultado.textContent = conversao.toLocaleString('pt-BR', { style: 'currency', currency: 'EUR' });
+    }
+
+}
+
+function inverter() {
+    let moeda1 = document.getElementById("moeda1").value;
+    let moeda2 = document.getElementById("moeda2").value;
+
+    document.getElementById("moeda1").value = moeda2;
+    document.getElementById("moeda2").value = moeda1;
+}
